f821805eeceadc0d00568d8860380f0eea840dc8
Nathaniel-Mercado <107744380+Nathaniel-Mercado@users.noreply.github.com>
2022-07-26 21:03:04 +0800

    revision

--- mod-2-ipa-1.py
+++ mod-2-ipa-1.py
@@ -70,6 +70,33 @@ def material_waste(total_material, material_units, num_jobs, job_consumption):
     # Stay within the function. Only use the parameters as input. The function should return your answer.
     return str(total_material - (num_jobs * job_consumption)) + material_units
 
+def interest(principal, rate, periods):
+    '''Interest.
+    5 points.
+    This function calculates the final value of an investment after
+        gaining simple interest over a number of periods.
+    To calculate simple interest, simply multiply the principal to the quantity (rate * time). 
+        Add this amount to the principal to get the final value.
+    Round down the final amount.
+    Parameters
+    ----------
+    principal: int
+        the principal (i.e., starting) amount invested, expressed in centavos
+    rate: float
+        the interest rate per period, expressed as a decimal representation of a percentage (e.g., 3% is 0.03)
+    periods: int
+        the number of periods invested
+    Returns
+    -------
+    int
+        the final value of the investment
+    '''
+    # Replace `pass` with your code. 
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    answer = 0
+    answer += principal+(principal * (rate * periods))
+    return answer
+
 def body_mass_index(weight, height):
     '''Body Mass Index.
     5 points.
